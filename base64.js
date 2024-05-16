f0ee2c67a98af2fbb8f0b552d0324a5ddd6bfe57
David Binns <dbinns.web+git@gmail.com>
2024-05-16 16:27:15 +0100

    Add text coding

--- base64.js
+++ base64.js
@@ -4,11 +4,23 @@ import { decode64, encode64 } from "./base64shared.js";
 
 const base64Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 
+const encoder = new TextEncoder();
+
+export function encodeText(string) {
+  return encode64(encoder.encode(string), base64Alphabet, "=");
+}
+
 export function encode(bytes) {
   return encode64(bytes, base64Alphabet, "=");
 }
 
-export function decode(bytes) {
-  return decode64(bytes, base64Alphabet, "=");
+export function decode(string) {
+  return decode64(string, base64Alphabet, "=");
+}
+
+const decoder = new TextDecoder();
+
+export function decodeText(string) {
+  return decoder.decode(decode64(string, base64Alphabet, "="));
 }
 
--- base64url.js
+++ base64url.js
@@ -4,10 +4,23 @@ import { decode64, encode64 } from "./base64shared.js";
 
 const base64urlAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
 
-export function encode(bytes) {
-  return encode64(bytes, base64urlAlphabet)
+const encoder = new TextEncoder();
+
+export function encodeText(string) {
+  return encode64(encoder.encode(string), base64urlAlphabet);
 }
 
-export function decode(bytes) {
-  return decode64(bytes, base64urlAlphabet)
+export function encode(bytes) {
+  return encode64(bytes, base64urlAlphabet);
 }
+
+export function decode(string) {
+  return decode64(string, base64urlAlphabet);
+}
+
+const decoder = new TextDecoder();
+
+export function decodeText(string) {
+  return decoder.decode(decode64(string, base64urlAlphabet));
+}
+
